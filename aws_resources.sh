56502908c3f8483abc909ab346b178a27cc05736
Jens <github@jinix.de>
2020-04-16 21:48:14 +0200

    Update bucket creation

--- aws_resources.sh
+++ aws_resources.sh
@@ -14,7 +14,8 @@ create_bucket () {
   if [ -z $(bucket_exists $BUCKET_NAME) ]
   then
     echo "Create bucket $BUCKET_NAME in region $AWS_DEFAULT_REGION"
-    $(aws s3api create-bucket --bucket $BUCKET_NAME --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION)
+    result=$(aws s3api create-bucket --bucket $BUCKET_NAME --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION 2>&1 | grep $1)
+    echo $result
   else
     echo "Bucket $BUCKET_NAME exists"
   fi
